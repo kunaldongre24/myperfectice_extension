3cd189925b4dd9377c385d04e6388b99f3295698
Arijit Paria <tutoriex2020@gmail.com>
2022-12-22 10:17:33 +0530

    Initial commit

---
+++ background.js
@@ -0,0 +1,10 @@
+/*
+    Author: Arijit Paria 
+    Subscribe @tutoriex on youtube to get more such scripts
+    Note:
+    This script is free to use, do not pay anyone anything.
+    To modify or redistribute, kindly follow the license agreement strictly.
+*/
+console.log("This is background Script @tutoriex (Arijit Paria)");
+
+
